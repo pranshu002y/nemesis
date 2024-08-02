95d976536e27e25e95959ccd498dfebff9aabc11
Pranshu Yadav <pranshu.yadav@venturepact.com>
2024-08-02 19:49:07 +0530

    About Page

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+const About = ()=>{
+    return(
+        <div class="container-about">
+      
+      
+       
+        
+        <div class="bottom-section">
+            <h2>STORY BEGINS</h2>
+           <p>Welcome to <strong>Nemesis</strong> your go-to community for sharing and discovering the best dresses, movies, and games. Whether you're looking for fashion inspiration, movie recommendations, or the latest in gaming, our platform brings together a vibrant community of enthusiasts who share their top picks.</p>
+        </div>
+        
+       
+    </div>
+    )
+}
+export default About;
--- src/Layout.js
+++ src/Layout.js
@@ -3,11 +3,13 @@ import Nav from './Nav';
 import Nemesis from './Nemesis';
 import Container from './Container';
 import Carousel from "./Carousel";
+import About from "./About";
 const Layout = ()=>{
     return(
         <div>
         <Nav/>
         <Nemesis/>
+        <About/>
         <Carousel/>
         <Container/>
          </div>
